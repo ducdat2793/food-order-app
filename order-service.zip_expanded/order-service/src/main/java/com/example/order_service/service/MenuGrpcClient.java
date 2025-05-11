94f916885d0d8efecbf88c80c7b23f65651e614e
Dat <60936602+ducdat2793@users.noreply.github.com>
2025-05-12 01:56:46 +0700

    Update code

--- order-service.zip_expanded/order-service/src/main/java/com/example/order_service/service/MenuGrpcClient.java
+++ order-service.zip_expanded/order-service/src/main/java/com/example/order_service/service/MenuGrpcClient.java
@@ -1,5 +1,11 @@
 package com.example.order_service.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
 import com.food.grpc.menu.MenuRequest;
@@ -14,15 +20,34 @@ public class MenuGrpcClient {
 
 	private final MenuServiceGrpc.MenuServiceBlockingStub stub;
 
-    public MenuGrpcClient() {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-            .usePlaintext()
-            .build();
-        stub = MenuServiceGrpc.newBlockingStub(channel);
-    }
+	@Autowired
+	public MenuGrpcClient(DiscoveryClient discoveryClient) {
+		MenuServiceGrpc.MenuServiceBlockingStub tempStub = null;
 
-    public MenuResponse getMenuById(long id) {
-        MenuRequest request = MenuRequest.newBuilder().setId(id).build();
-        return stub.getMenuItem(request);
-    }    
+		List<ServiceInstance> instances = discoveryClient.getInstances("menu-service");
+		// System.out.println("====> "+ instances);
+		if (instances != null && !instances.isEmpty()) {
+			ServiceInstance instance = instances.get(0);
+			String host = instance.getHost();
+			// int port = instance.getPort();
+			
+			Map<String, String> metadata = instance.getMetadata();
+			int grpcPort = Integer.parseInt(metadata.get("grpc.port"));
+
+			ManagedChannel channel = ManagedChannelBuilder.forAddress(host, grpcPort).usePlaintext().build();
+
+			tempStub = MenuServiceGrpc.newBlockingStub(channel);
+		}
+
+		this.stub = tempStub;
+	}
+
+	public MenuResponse getMenuById(long id) {
+		if (stub == null) {
+			throw new IllegalStateException("gRPC stub is not initialized (menu-service not available)");
+		}
+		MenuRequest request = MenuRequest.newBuilder().setId(id).build();
+		return stub.getMenuItem(request);
+	}
+
 }
